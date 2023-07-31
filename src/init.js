be85939fb3955d8bc50bc981eda16ce713236682
himanshisaxena <saxena.himanshi11@gmail.com>
2023-07-31 15:17:41 +0200

    add code

--- src/init.js
+++ src/init.js
@@ -1,3 +1,12 @@
-import './components/createItem.js';
-import './components/themeSwitcher.js';
+import { data, dom } from './data.js';
+import createItem from './components/createItem.js';
 
+// default show some items
+data.items.forEach((itemData) => {
+    const item = createItem(itemData);
+    dom.itemsList.append(item);
+});
+
+import './listeners/addItemListener.js';
+import './listeners/submitItemListener.js';
+import './listeners/deleteItemsListener.js';
